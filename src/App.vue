527436cf6cfaea9b8ecdc806d4d2be25465ae328
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2022-01-08 11:02:53 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer temporary v-model="drawer" app>
+      <v-list>
+        <v-list-item v-for="item in navigations" :key="'page-' + item.text" :to="item.link">
+          <v-list-item-title>{{ item.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        <router-link class="white--text" to="/">Pokedex</router-link>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view :key="$route.fullPath"/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  name: 'App',
+  data: () => {
+    return {
+      drawer: false,
+      navigations: [
+        {
+          text: 'Home',
+          link: '/',
+        },
+      ],
+    };
   }
+}
+</script>
+
+<style>
+*, *:hover, *:focus, *:active {
+  cursor: url('/gam1284.cur'), pointer;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <!-- Pokemon grid -->
+    <v-container fluid>
+      <v-row>
+        <v-col cols="4" lg="3" v-for="item in pokemons" :key="'pokemonId-' + item.id">
+          <v-hover v-slot="{ hover }">
+            <v-card elevation="4" class="rounded-xl pa-5 mx-auto" ripple>
+              <v-img
+                aspect-ratio="1"
+                contain
+                class="justify-center align-center"
+                height="220"
+                :src="'http://localhost:8000/img/pokemon/' + item.image"
+              >
+              </v-img>
+                <v-fade-transition>
+                  <v-overlay v-if="hover" absolute color="black">
+                    <v-card-title
+                      class="justify-center text-h6 font-weight-bold"
+                    >{{ item.name }}</v-card-title>
+                  </v-overlay>
+                </v-fade-transition>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+  },
+  data: () => ({
+    pokemons: null,
+  }),
+  mounted () {
+    axios
+      .get('http://localhost:8000/api/v1/pokemon')
+      .then(res => this.pokemons = res.data.data)
+      .then(err => console.log(err));
+  },
 }
 </script>
+
+<style>
+</style>
